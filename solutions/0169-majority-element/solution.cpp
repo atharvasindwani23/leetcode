828f57163eb4843ea4cc7bd77eb167fb198899ee
Atharva Sindwani <arvani@amazon.com>
2024-06-28 10:15:39 +0000

    Sync LeetCode submission Runtime - 20 ms (6.53%), Memory - 26.8 MB (99.97%)

--- solutions/0169-majority-element/solution.cpp
+++ solutions/0169-majority-element/solution.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n/2];
+       std::map<int,int> num_freq;
+       for (int i  : nums) {
+        if (!num_freq.contains(i)) {
+        num_freq[i] = 1;
+        } else {
+            num_freq[i]++;
+        }
+       }
+       for (int i : nums) {
+        if (num_freq[i] > nums.size() / 2) {
+            return i;
+        }
+       }
+       return 0;
     }
 };
