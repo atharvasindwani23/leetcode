c0eb3b0a6e47228f2f78a32a50472aeecd1f6805
Atharva Sindwani <arvani@amazon.com>
2024-07-31 09:18:39 +0000

    Sync LeetCode submission Runtime - 124 ms (5.09%), Memory - 12.7 MB (99.99%)

--- solutions/0001-two-sum/solution.cpp
+++ solutions/0001-two-sum/solution.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> returner;
-        for (unsigned int i  = 0;  i < nums.size(); i++) {
-            for (unsigned int j = 0; j < nums.size(); j++) {
-            if (nums[i] + nums[j] == target &&  i < j) {
-             returner.push_back(i);
-             returner.push_back(j);
-            }
+        for (unsigned int i = 0; i < nums.size(); i++) {
+            for (unsigned int j = i + 1; j < nums.size(); j++) {
+                if (target == nums[j] + nums[i]) {
+                    returner.push_back(i);
+                    returner.push_back(j);
+                }
             }
         }
         return returner;
