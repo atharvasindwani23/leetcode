9da096d7bda584c619e5bc48f7367c58484a920a
Atharva Sindwani <arvani@amazon.com>
2024-08-17 21:40:33 +0000

    Sync LeetCode submission Runtime - 5 ms (0.69%), Memory - 8.9 MB (100.00%)

--- solutions/0048-rotate-image/solution.cpp
+++ solutions/0048-rotate-image/solution.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>>  copier = copy(matrix);
-        for (unsigned int i = 0; i < matrix.size(); i++) {
-            for (unsigned int j = 0; j < matrix.size(); j++) {
-              matrix.at(i).at(j) = copier.at(matrix.size() - j - 1).at(i);
-        }
-    }
-}
- vector<vector<int>> copy(const vector<vector<int>>& matrix) {
-    vector<vector<int>> returner(matrix.size(), vector<int>(matrix.size()));
-    for (unsigned int i = 0; i < matrix.size(); i++) {
+     vector<vector<int>> t = matrix;
+     for (unsigned int i = 0; i < matrix.size(); i++) {
         for (unsigned int j = 0; j < matrix.size(); j++) {
-            returner[i][j] = matrix[i][j];
+            matrix[j][matrix.size() - i - 1] = t[i][j];
         }
+     }
     }
-    return returner;
-}
 };
