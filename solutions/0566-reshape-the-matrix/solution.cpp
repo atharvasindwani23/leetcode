1993e42ff8be9fc33e747564e07ec707c6920506
Atharva Sindwani <arvani@amazon.com>
2024-08-17 22:06:58 +0000

    Sync LeetCode submission Runtime - 8 ms (2.36%), Memory - 13.4 MB (100.00%)

--- solutions/0566-reshape-the-matrix/solution.cpp
+++ solutions/0566-reshape-the-matrix/solution.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        if (r * c != mat.size() * mat.at(0).size()) {
-            return mat;
+     if (r * c  != mat.size() * mat.at(0).size()) {
+        return mat;
+     }
+     vector<vector<int>> returner;
+     vector<int> flag;
+     for (unsigned int i = 0; i < mat.size(); i++) {
+        for (unsigned int j = 0; j < mat.at(0).size(); j++) {
+          flag.push_back(mat[i][j]);
+          if (flag.size() == c) {
+            returner.push_back(flag);
+            flag.clear();
+          }
         }
-       int count_i = 0;
-       int count_j = 0;
-vector<vector<int>> returner(r, vector<int>(c));
-        for (unsigned int i = 0; i < r; i++) {
-            for (unsigned int j = 0; j < c ; j++) {
-                returner[i][j] = mat.at(count_i).at(count_j);
-                count_j++;
-                if (count_j >= mat.at(0).size()) {
-                    count_i++;
-                    count_j = 0;
-                }
-            }
-        }
-        return returner;
+     }
+     return returner;
     }
 };
