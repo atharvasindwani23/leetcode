6a5e2a759ef60f2688296f62c379e102039f26ae
Atharva Sindwani <arvani@amazon.com>
2024-08-18 08:36:53 +0000

    Sync LeetCode submission Runtime - 3 ms (4.35%), Memory - 13.6 MB (100.00%)

--- solutions/0898-transpose-matrix/solution.cpp
+++ solutions/0898-transpose-matrix/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-vector<vector<int>> returner(matrix.at(0).size(), vector<int>(matrix.size()));
-        for (unsigned int i = 0; i < matrix.at(0).size(); i++) {
-            for (unsigned int j = 0; j < matrix.size(); j++) {
-             returner[i][j] = matrix[j][i];
-            }
+    vector<vector<int>> returner(matrix.at(0).size(), vector<int>(matrix.size()));
+    for (unsigned int i = 0; i < matrix.at(0).size(); i++) {
+        for (unsigned int j = 0; j < matrix.size(); j++) {
+          returner[i][j] = matrix[j][i];
         }
-        return returner;
+    }
+    return returner;
     }
 };
