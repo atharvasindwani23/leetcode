9435f82b8c5e84502eae4e4db30f91f52e2ebe94
Atharva Sindwani <arvani@amazon.com>
2024-08-18 08:32:46 +0000

    Sync LeetCode submission Runtime - 16 ms (2.51%), Memory - 22 MB (5.02%)

--- solutions/0777-toeplitz-matrix/solution.cpp
+++ solutions/0777-toeplitz-matrix/solution.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        return columnHelper(matrix);
+    for (unsigned int i  = 0; i < matrix.at(0).size(); i++) {
+        int temp = i;
+        int count = 0;
+        while (count < matrix.size()) {
+        try {
+        if (matrix.at(0).at(i) != matrix.at(count).at(temp)) {
+            std::cout << "huh";
+            return false;
+        }
+        count++;
+        temp++;
+        } catch(...){
+            break;
+        }
+        }
     }
-    bool columnHelper(vector<vector<int>>& matrix) {
-        for (unsigned int i =  0; i < matrix.size() - 1; i++) {
-            for (unsigned int j = 0; j < matrix.at(i).size() - 1; j++) {
-                if (matrix[i][j] != matrix[i + 1][j + 1]) {
-                     return false;
-                  }
-            }
+    for (unsigned int i = 1; i < matrix.size(); i++) {
+        int temp = i;
+        int count = 0;
+        while (count < matrix.size()) {
+        try {
+        if (matrix.at(i).at(0) != matrix.at(temp).at(count)) {
+            return false;
+        }
+        count++;
+        temp++;
+        } catch(...){
+            break;
+        }
+    }
     }
     return true;
-}
+    }
 };
