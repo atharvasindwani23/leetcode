11cbfc12df18ef53f9dd1deb9facee265e663335
Atharva Sindwani <arvani@amazon.com>
2024-08-19 08:41:59 +0000

    Sync LeetCode submission Runtime - 69 ms (79.92%), Memory - 49.8 MB (100.00%)

--- solutions/0128-longest-consecutive-sequence/solution.cpp
+++ solutions/0128-longest-consecutive-sequence/solution.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-      std::sort(nums.begin(), nums.end());
-      if (nums.size() == 0) {
+    int returner = 0;
+    int count = 0;
+    if (nums.size() == 0) {
         return 0;
-      }
-      //std::cout << nums.at(0);
-      int count = 0;
-      int max = 0;
-      for (unsigned int i = 0; i < nums.size() - 1; i++) {
-        if (nums[i + 1] - nums[i] == 1) {
-            count++;
+    }
+    std::sort(nums.begin(), nums.end());
+    for (unsigned int i = 1; i < nums.size(); i++) {
+       if (nums[i] - nums[i - 1] == 1) {
+        count++;
+       } else {
+        if (nums[i] - nums[i -1] == 0) {
+            ;
         } else {
-            if (nums[i + 1] - nums[i] == 0) {
-               
-            } else {
-            max = std::max(max, count);
-            count = 0;
-            }
+        returner = std::max(returner, count);
+        count = 0;
         }
-      max = std::max(max, count);
-      }
-      return max + 1;
+       }   
+    }
+    returner = std::max(returner, count);
+    return returner + 1;
     }
 };
