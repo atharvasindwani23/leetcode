13e3ab5c28cc40ebef25a8088fb7e4ab1fe1c250
Atharva Sindwani <arvani@amazon.com>
2025-03-16 21:13:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.2 MB (52.45%)

--- solutions/0202-happy-number/solution.cpp
+++ solutions/0202-happy-number/solution.cpp
@@ -1,24 +1,21 @@
 class Solution {
+private:
+    int nextNumber(int n){
+        int newNumber = 0;
+        while(n!=0){
+            int num = n%10;
+            newNumber += num*num;
+            n = n/10;
+        }
+        return newNumber;
+    }
 public:
- bool isHappy(int n) {
-    // Use Floyd's Cycle-Finding Algorithm (Tortoise and Hare)
-    int slow = n;
-    int fast = squareReplacer(squareReplacer(n));
-
-    while (fast != 1 && slow != fast) {
-        slow = squareReplacer(slow); // Move slow pointer one step
-        fast = squareReplacer(squareReplacer(fast)); // Move fast pointer two steps
-    }
-
-    return fast == 1; // If fast reaches 1, it's a happy number; otherwise, it's not
-}
-    int squareReplacer(int n) {
-         int sum = 0;
-         while (n > 0) {
-         int digit = n % 10;  // Extract the last digit
-         sum += digit * digit;  // Square the digit and add to sum
-          n /= 10;             // Remove the last digit
-    }
-    return sum;
+    bool isHappy(int n) {
+        unordered_set<int> set;
+        while(n!=1 && !set.count(n)){
+            set.insert(n);
+            n = nextNumber(n);
+        }
+        return n==1;
     }
 };
