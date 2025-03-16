47562676aac47bf2a0e9081c3b5abd516982cb19
Atharva Sindwani <arvani@amazon.com>
2025-03-16 21:18:59 +0000

    Sync LeetCode submission Runtime - 7 ms (18.89%), Memory - 9.7 MB (75.08%)

--- solutions/0242-valid-anagram/solution.cpp
+++ solutions/0242-valid-anagram/solution.cpp
@@ -1,30 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) {
-            return false;
-        }
-        std::map<char, int> freq;
-        std::map<char, int> freq_2;
-        for (unsigned int i  = 0; i < s.size(); i++) {
-            if (freq.contains(s[i])) {
-                freq[s[i]]++;
-            } else {
-                freq[s[i]] = 1;
-            }
-        }
-        for (unsigned int i  = 0; i < t.size(); i++) {
-            if (freq_2.contains(t[i])) {
-                freq_2[t[i]]++;
-            } else {
-                freq_2[t[i]] = 1;
-            }
-        }
-        for (unsigned int i = 0; i < t.size(); i++) {
-             if (freq.contains(t[i]) == false || freq[t[i]] != freq_2[t[i]]) {
-                return false;
-             }
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
