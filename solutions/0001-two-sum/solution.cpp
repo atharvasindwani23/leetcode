4a4719792925efa689087252d7eb9be62367c518
Atharva Sindwani <arvani@amazon.com>
2025-03-17 01:27:27 +0000

    Sync LeetCode submission Runtime - 15 ms (35.82%), Memory - 16.4 MB (5.30%)

--- solutions/0001-two-sum/solution.cpp
+++ solutions/0001-two-sum/solution.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> returner;
-        for (unsigned int i = 0; i < nums.size(); i++) {
-            for (unsigned int j = i + 1; j < nums.size(); j++) {
-                if (target == nums[j] + nums[i]) {
-                    returner.push_back(i);
-                    returner.push_back(j);
-                }
-            }
-        }
-        return returner;
+    std::map<int, int> values;
+    for (unsigned int i = 0; i < nums.size(); i++) {
+        values[nums[i]] = i; 
+    }
+    std::vector<int> returner;
+    for (unsigned int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (values.find(complement) == values.end()) {
+            values[nums[i]] = i;
+        } else {
+           returner.push_back(i);
+           returner.push_back(values[target - nums[i]]);
+           if (i == values[target - nums[i]]) {
+            returner.clear();
+            continue;
+           }
+           return returner;
+        }   
+    }
+     return returner;
     }
 };
