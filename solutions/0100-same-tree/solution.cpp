79a27de09ffb7e381b2edbd1e03a061bc8f8298d
Atharva Sindwani <arvani@amazon.com>
2025-03-21 03:37:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13 MB (12.60%)

--- solutions/0100-same-tree/solution.cpp
+++ solutions/0100-same-tree/solution.cpp
@@ -14,14 +14,13 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (!p && !q) {
             return true;
-        } else if (p && q) {
-            if (p->val == q->val) {
-            return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
-            } else {
-                return false;
-            }
-        } else {
+        }
+        if (p && !q) {
             return false;
         }
+        if (!p && q) {
+            return false;
+        }
+        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right) && (p->val == q->val));
     }
 };
