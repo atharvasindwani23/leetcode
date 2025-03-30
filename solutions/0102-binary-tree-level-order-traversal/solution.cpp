8a300c19c75f518c2aa347d9022de2e1a98fe3e2
Atharva Sindwani <arvani@amazon.com>
2025-03-30 09:50:51 +0000

    Sync LeetCode submission Runtime - 4 ms (13.29%), Memory - 17.1 MB (71.68%)

--- solutions/0102-binary-tree-level-order-traversal/solution.cpp
+++ solutions/0102-binary-tree-level-order-traversal/solution.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> returner;
-        if (!root) {
-            return returner;
-        }
-        std::queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> flag;
-            for (int i = 0; i < size; i++) {
-              TreeNode* temp = q.front();
-              if (temp->left) {
-                q.push(temp->left);
-              }
-              if (temp->right) {
-                q.push(temp->right);
-              }
-              q.pop();
-              flag.push_back(temp->val);
+       vector<vector<int>> returner;
+       if (!root) {
+        return returner;
+       }
+        queue<TreeNode*> bfs;
+        bfs.push(root);
+        while (!bfs.empty()) {
+            int sizeQ = bfs.size();
+            vector<int>flag;
+            for (int i  = 0; i < sizeQ; i++) {
+                TreeNode* temp = bfs.front();
+                bfs.pop();
+                flag.push_back(temp->val);
+                if (temp->left) {
+                    bfs.push(temp->left);
+                }
+                if (temp->right) {
+                    bfs.push(temp->right);
+                }
             }
             returner.push_back(flag);
         }
