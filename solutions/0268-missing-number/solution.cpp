c29bbeb61783c79d47fd6cf190d6451224159d1b
Atharva Sindwani <arvani@amazon.com>
2025-06-05 22:27:14 +0000

    Sync LeetCode submission Runtime - 39 ms (5.03%), Memory - 29.5 MB (5.28%)

--- solutions/0268-missing-number/solution.cpp
+++ solutions/0268-missing-number/solution.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        std::map<int,bool> present;
-        for (unsigned int i  = 0; i < nums.size(); i++) {
-             present[nums[i]] = true;
+        std::map<int,int> freq;
+        for (int i : nums) {
+            freq[i]++;
         }
-        for (int i = 0; i < nums.size() + 1; i++) {
-            if (!present[i]) {
+        for (int i = 0; i <= nums.size(); i++) {
+            if (freq[i] == 0) {
                 return i;
             }
         }
         return -1;
     } 
 };
- // nums.size() == n - 1;
-    // are in any order
-    // sort the array -> [0 , 1, 3]
-    // for (for every element) {
-    // its index and its value
-    //i != arr[i]
-    // [0 1 2] -> return nums.size()
-    //  O(nlogn) -> ideally should do it one traversal
-    // std::map<int, bool> -> default value of false
-    // map within the loop -> nums.size() elements nums.size() + 1 -> x.second == false;
+
