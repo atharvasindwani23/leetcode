9a151d2c7b4fbf3d9224de1b652d861991db4d5a
Atharva Sindwani <arvani@amazon.com>
2025-06-05 22:17:11 +0000

    Sync LeetCode submission Runtime - 107 ms (7.21%), Memory - 94.8 MB (5.18%)

--- solutions/0217-contains-duplicate/solution.cpp
+++ solutions/0217-contains-duplicate/solution.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        for (unsigned int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i + 1] - nums[i] == 0) {
+        std::map<int,int> freq;
+        for (int i  : nums) {
+            freq[i]++;
+        }
+        for (auto x = freq.begin(); x!= freq.end(); x++) {
+            if (x->second != 1) {
                 return true;
             }
         }
@@ -12,10 +15,4 @@ public:
 };
 
 
-// [1 2 3 1] => true
-// [1 2 1 2] => true
-// [] => false
-// looping over twice -> arr[i] == arr[j] where our i != j; -> BRUTE FORCE SOLUTION 
-// O(n^2) time
-// hashMap of some sort -> .count() > 1 -> return true, O(n) solution
 
