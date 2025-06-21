bc6e3046da3df438fa042a38cd1650b65f7e047d
Atharva Sindwani <arvani@amazon.com>
2025-06-21 22:41:15 +0000

    Sync LeetCode submission Runtime - 175 ms (6.92%), Memory - 95.5 MB (5.16%)

--- solutions/0128-longest-consecutive-sequence/solution.cpp
+++ solutions/0128-longest-consecutive-sequence/solution.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-    int returner = 0;
-    int count = 0;
-    if (nums.size() == 0) {
-        return 0;
-    }
-    std::sort(nums.begin(), nums.end());
-    for (unsigned int i = 1; i < nums.size(); i++) {
-       if (nums[i] - nums[i - 1] == 1) {
-        count++;
-       } else {
-        if (nums[i] - nums[i -1] == 0) {
-            ;
-        } else {
-        returner = std::max(returner, count);
-        count = 0;
+        if (nums.size() == 0) {
+            return 0;
         }
-       }   
-    }
-    returner = std::max(returner, count);
-    return returner + 1;
+        
+
+        sort(nums.begin(), nums.end());
+        // 100,4,200,1,3,2 => 1,2,3,4,100,200
+        //0,0,1,1,2,2,2,3,4
+        std::map<int,bool> present;
+        std::vector<int> newVec;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!present[nums[i]]) {
+                present[nums[i]] = true;
+                newVec.push_back(nums[i]);
+            }
+        }
+        
+        int longest = 1;
+        int maximum = 1;
+        int lower = 0;
+        int upper = 1;
+        while (upper < newVec.size()) {
+            if (newVec[upper] - newVec[lower] == upper - lower) {
+                upper++;
+                longest++;
+            } else {
+                maximum = max(maximum, longest);
+                lower = upper;
+                upper++;
+                longest = 1;
+            }
+        }
+    
+        return max(maximum, longest);
     }
 };
