04424a6ae0f218c8069748d0a758a0df80cee4e9
Atharva Sindwani <arvani@amazon.com>
2025-06-26 05:13:54 +0000

    Sync LeetCode submission Runtime - 3 ms (14.64%), Memory - 24.9 MB (5.20%)

--- solutions/0283-move-zeroes/solution.cpp
+++ solutions/0283-move-zeroes/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        std::vector<int> returner;
+        vector<int> zeroes;
         int c = 0;
-        for (unsigned int i = 0; i < nums.size(); i++) {
-             if (nums[i] != 0) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
                 c++;
-                returner.push_back(nums[i]);
-             }
+                continue;
+            }
+            zeroes.push_back(nums[i]);
         }
-        for (unsigned int i = 0; i < nums.size() - c; i++) {
-            returner.push_back(0);
+        for (int i = 0; i < c; i++) {
+            zeroes.push_back(0);
         }
-        std::cout << c;
-        nums = returner;
+        nums = zeroes;
     }
 };
