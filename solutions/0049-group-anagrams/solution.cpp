edd7eeb17061bb4f44d3a41721273d3f89f21767
Atharva Sindwani <arvani@amazon.com>
2025-06-26 05:39:06 +0000

    Sync LeetCode submission Runtime - 22 ms (32.95%), Memory - 25 MB (79.04%)

--- solutions/0049-group-anagrams/solution.cpp
+++ solutions/0049-group-anagrams/solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        
-        for(auto x: strs){
-            string word = x;
+        std::map<string, vector<string>> anagrams;
+        for (string str : strs) {
+            string word = str;
             sort(word.begin(), word.end());
-            mp[word].push_back(x);
+            anagrams[word].push_back(str);
         }
-        
-        vector<vector<string>> ans;
-        for(auto x: mp){
-            ans.push_back(x.second);
+        vector<vector<string>> returner;
+        for (auto x = anagrams.begin(); x != anagrams.end(); x++) {
+            returner.push_back(x->second);
         }
-        return ans;
+        return returner;
     }
-
 };
