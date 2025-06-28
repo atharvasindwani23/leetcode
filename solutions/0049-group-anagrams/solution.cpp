c30cbd927e8a4f30edc4fea9acb688a9948e3336
Atharva Sindwani <arvani@amazon.com>
2025-06-28 02:18:24 +0000

    Sync LeetCode submission Runtime - 21 ms (34.87%), Memory - 25 MB (67.40%)

--- solutions/0049-group-anagrams/solution.cpp
+++ solutions/0049-group-anagrams/solution.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         std::map<string, vector<string>> anagrams;
-        for (string str : strs) {
+        vector<vector<string>> returner;
+        for (string str: strs) {
             string word = str;
             sort(word.begin(), word.end());
             anagrams[word].push_back(str);
         }
-        vector<vector<string>> returner;
-        for (auto x = anagrams.begin(); x != anagrams.end(); x++) {
+        for (auto x = anagrams.begin(); x!= anagrams.end(); x++) {
             returner.push_back(x->second);
         }
         return returner;
     }
+    //anagrams are essentially strings with the exact same length and frequency of each character
+    // 123, 321 => 123, 123. 
+    //anarams will have a special property which is => upon sorting theyll be equal
+    //o(n^3) => complexity
+    //hashMap => [][][][] => key = sorted string, value be the anagram which upon sorting matches the key
+    //at the end ill just print out the values
+    // [eat, tea, pea, eap]
+    // aet-> [eat, tea], aep -> [pea, eap]
 };
