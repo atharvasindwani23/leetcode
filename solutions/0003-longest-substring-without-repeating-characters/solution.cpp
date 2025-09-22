1cba709605185056d1de14338148b8bae58a3b34
Atharva Sindwani <arvani@amazon.com>
2025-09-22 04:56:23 +0000

    Sync LeetCode submission Runtime - 18 ms (30.09%), Memory - 14.4 MB (20.82%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.cpp
+++ solutions/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if (s.size() == 0) {
-            return 0;
-        }
-        if (s.size() == 1) {
-            return 1;
-        }
-        int first = 0;
-        int next = 1;
+        int n = s.length();
         int maxLength = 0;
-        set<int> unique;
-        unique.insert(s[first]);
- 
-        while (next < s.size()) {
-         if (unique.contains(s[next])) {
-           unique.erase(s[first]);
-           first++;
-         } else {
-            unique.insert(s[next]);
-            maxLength = max(maxLength, next - first + 1);
-            next++;
-         }
+        unordered_set<char> charSet;
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            if (charSet.count(s[right]) == 0) {
+                charSet.insert(s[right]);
+                maxLength = max(maxLength, right - left + 1);
+            } else {
+                while (charSet.count(s[right])) {
+                    charSet.erase(s[left]);
+                    left++;
+                }
+                charSet.insert(s[right]);
+            }
         }
+        
         return maxLength;
     }
 };
-
-
-// b a c d a e ->   //two pointers 
-// a b c d
-// a b 
-// a a
-// a
-// ""
-
-//
-
