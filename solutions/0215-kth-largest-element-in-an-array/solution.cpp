f6eee43d3318bc8a326ca34235b12996ffb7c478
Atharva Sindwani <arvani@amazon.com>
2025-09-22 05:21:55 +0000

    Sync LeetCode submission Runtime - 128 ms (6.62%), Memory - 76.3 MB (10.12%)

--- solutions/0215-kth-largest-element-in-an-array/solution.cpp
+++ solutions/0215-kth-largest-element-in-an-array/solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (int num : nums) {
-            pq.push(num);
+        map<int, int> freq;
+        for (int i  = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
         }
-        for (int i = 0; i < nums.size() - k; i++) {
+        priority_queue<int> pq;
+        for (auto x = freq.begin(); x != freq.end(); x++) {
+            for (int i  = 0 ; i < x->second; i++) {
+                pq.push(x->first);
+            }
+        }
+        for (int i = 0; i < k - 1; i++) {
             pq.pop();
         }
         return pq.top();
     }
-
-
-    // [1 2 3 4 5 6] -> 2nd largest = 5th smallest, nth largest = (size - n + 1)th smallest
-    //pop size - n times then?
 };
