dfbad8ba4532e8ac9299a76e57b44e018f05b547
Atharva Sindwani <arvani@amazon.com>
2025-09-25 16:07:16 +0000

    Sync LeetCode submission Runtime - 4 ms (51.24%), Memory - 15 MB (18.53%)

--- solutions/0001-two-sum/solution.cpp
+++ solutions/0001-two-sum/solution.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    std::map<int, int> values;
-    for (unsigned int i = 0; i < nums.size(); i++) {
-        values[nums[i]] = i; 
-    }
-    std::vector<int> returner;
-    for (unsigned int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
-        if (values.find(complement) == values.end()) {
-            values[nums[i]] = i;
-        } else {
-           returner.push_back(i);
-           returner.push_back(values[target - nums[i]]);
-           if (i == values[target - nums[i]]) {
-            returner.clear();
-            continue;
-           }
-           return returner;
-        }   
-    }
-     return returner;
+        map<int,int> store;
+        vector<int> res;
+        //create a map of differences -> map[sum - arr[i]] = i
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (store.count(diff) == 0) {
+                store[nums[i]] = i;
+            } else {
+                res.push_back(i);
+                res.push_back(store[diff]);
+                return res;
+            }
+        }
+        return {};
     }
 };
