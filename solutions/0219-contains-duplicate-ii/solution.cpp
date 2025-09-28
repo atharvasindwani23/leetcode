aee7167872c3f8d06932e9de6f51dab2dcc48777
Atharva Sindwani <arvani@amazon.com>
2025-09-28 17:46:48 +0000

    Sync LeetCode submission Runtime - 154 ms (14.10%), Memory - 102.4 MB (7.20%)

--- solutions/0219-contains-duplicate-ii/solution.cpp
+++ solutions/0219-contains-duplicate-ii/solution.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-       set<int> unique;
-       int first = 0;
-       int next = 1;
-       unique.insert(nums[first]);
-       while (next < nums.size()) {
-           if (unique.contains(nums[next]) && next - first <=k) {
-            std::cout << next << std::endl;
-            std::cout << first << std::endl;
-            return true;
-           }
-           while (next - first > k) {
-            std::cout << next << std::endl;
-            std::cout << first << std::endl;
-            std::cout << "nigger?" << std::endl;
-            unique.erase(nums[first]);
-            first++;
-           }
-           if (!unique.contains(nums[next])) {
-            unique.insert(nums[next]);
-            next++;
-           }
-       }
-       return false;
+        map<int, int> mostRecIdx;
+        for (int i = 0; i < nums.size(); i++) {
+            int curr = nums[i];
+            if (mostRecIdx.count(curr)) {
+                if (i - mostRecIdx[curr] <= k) {
+                    return true;
+                }
+            }
+            mostRecIdx[curr] = i;
+        }
+        return false; 
     }
+    
+
+    //can we do a two pointer approach?
+    // do we store the indexes in a sort of a map and then sort?
+    // what i mean by that is we make a map<int, vector<int>> if vector<int>.size() >= 2, this means that there are indexes stored
+    //we single out the elements which have the vectors whose sizes are greater and then we do something maybe?
+
+    // the problem is the confusion of knowing where to move if two things dont meet
+    // say nums[l] != nums[r] now what? which pointer do we move? we don't have enough info on this unfortunately
+
+    //can't sort because thers the index difference constraint
+    // what can we do instead?
+
 };
