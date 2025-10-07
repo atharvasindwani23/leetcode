e32d72db8894186b1060c596286836147cee11fc
Atharva Sindwani <arvani@amazon.com>
2025-10-07 20:48:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 24.1 MB (4.24%)

--- solutions/0303-range-sum-query---immutable/solution.cpp
+++ solutions/0303-range-sum-query---immutable/solution.cpp
@@ -1,17 +1,28 @@
 class NumArray {
 public:
-    vector<int> vec;
+
+//can do it by prefix and suffix if we are really smart
+    //make an array input such that input[i] has all the sums from
+    vector<int> input;
     NumArray(vector<int>& nums) {
-        vec = nums;
+        input.resize(nums.size());  
+        input[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            input[i] = input[i - 1] + nums[i];
+        }
     }
     
     int sumRange(int left, int right) {
-        int sum = 0;
-        for (int i = left; i <= right; i++) {
-          sum += vec[i]; 
+        //input[left] = nums[0] + .... nums[left]
+        //input[right] = nums[0] + ...................nums[right]
+        if (left == 0) {
+            return input[right];
         }
-        return sum;
+      return (input[right] - input[left - 1]);
     }
+    //[-2, -2, 1, -4, -2, -3]
+    // 0       2           5 
+    //is there a way to do this in o(1)
 };
 
 /**
