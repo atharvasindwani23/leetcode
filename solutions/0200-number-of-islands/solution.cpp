4421fe34b58ee61f0c94b9a6a19a3caa888db2ac
Atharva Sindwani <arvani@amazon.com>
2025-12-10 02:08:16 +0000

    Sync LeetCode submission Runtime - 25 ms (72.47%), Memory - 16 MB (96.10%)

--- solutions/0200-number-of-islands/solution.cpp
+++ solutions/0200-number-of-islands/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int res = 0;
-        for (int i  = 0; i < grid.size(); i++) {
+        int result = 0;
+        for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == '1') {
-                    res++;
                     dfs(grid, i, j);
+                    result++;
                 }
             }
         }
-        return res;
+        return result;
     }
 
     void dfs(vector<vector<char>>& grid, int i, int j) {
