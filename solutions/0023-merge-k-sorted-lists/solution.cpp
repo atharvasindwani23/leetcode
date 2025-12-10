a88f27bbf83b543c001beb38fd4072e0cc9cdce3
Atharva Sindwani <arvani@amazon.com>
2025-12-10 02:34:53 +0000

    Sync LeetCode submission Runtime - 23 ms (15.58%), Memory - 18.4 MB (79.94%)

--- solutions/0023-merge-k-sorted-lists/solution.cpp
+++ solutions/0023-merge-k-sorted-lists/solution.cpp
@@ -13,28 +13,28 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.empty()) {
             return nullptr;
-        }
+        }   
         while (lists.size() > 1) {
-            lists.push_back(mergeSortedList(lists[0], lists[1]));
+            lists.push_back(mergeSortList(lists[0], lists[1]));
             lists.erase(lists.begin());
             lists.erase(lists.begin());
         }
-        return lists.front();
+        return lists[0];
     }
 
-    ListNode* mergeSortedList(ListNode* list1, ListNode* list2) {
-        if (list1 == nullptr) {
-            return list2;
+    ListNode* mergeSortList(ListNode* l1, ListNode* l2) {
+        if (!l2) {
+            return l1;
         }
-        if (list2 == nullptr) {
-            return list1;
+        if (!l1) {
+            return l2;
         }
-        if (list1->val <= list2->val) {
-            list1->next = mergeSortedList(list1->next, list2);
-            return list1;
+        if (l1->val <= l2->val) {
+            l1->next = mergeSortList(l1->next, l2);
+            return l1;
         } else {
-            list2->next = mergeSortedList(list1, list2->next);
-            return list2;
+            l2->next = mergeSortList(l1, l2->next);
+            return l2;
         }
     }
 };
