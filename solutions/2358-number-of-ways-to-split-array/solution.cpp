14274214334513c20264914cd7571872072bce11
Atharva Sindwani <arvani@amazon.com>
2025-12-11 19:02:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 89.3 MB (97.11%)

--- solutions/2358-number-of-ways-to-split-array/solution.cpp
+++ solutions/2358-number-of-ways-to-split-array/solution.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-       long totalSum = 0;
+        long long runningSum = 0;
+        long long totalSum = 0;
 
-       for (int num : nums) {
-        totalSum += num;
-       } 
-       long runningSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
         int count = 0;
-       for (int i = 0; i < nums.size() - 1; i++) {
-            runningSum = runningSum + nums[i];
-            if (runningSum * 2 >= totalSum) {
-                cout << i << endl;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            runningSum += nums[i];
+            if (runningSum >= totalSum - runningSum) {
                 count++;
             }
-       }
-       return count;
+        }
+        return count;
     }
-
 };
