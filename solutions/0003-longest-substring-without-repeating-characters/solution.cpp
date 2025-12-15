b4aa3b9416e2c85662d135e449d623a3aae04f39
Atharva Sindwani <arvani@amazon.com>
2025-12-15 22:10:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.7 MB (66.11%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.cpp
+++ solutions/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int start = 0;
-        unordered_map<char, int> lastSeen;
         int maxLen = 0;
-
+        unordered_map<char, int> lastSeen;
         for (int i = 0; i < s.size(); i++) {
             char ch = s[i];
-
             if (lastSeen.count(ch) && lastSeen[ch] >= start) {
                 start = lastSeen[ch] + 1;
             }
             lastSeen[ch] = i;
             maxLen = max(maxLen, i - start + 1);
         }
-        return maxLen;
+        return maxLen;      
     }
 };
