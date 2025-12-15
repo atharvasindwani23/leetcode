c5943805a61a1d5e2838661b9518225162cb9d17
Atharva Sindwani <arvani@amazon.com>
2025-12-15 21:52:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 97.2 MB (99.31%)

--- solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-     int l = 0;
-     int r = 1;
-     int maxProf = 0;
-     while (r < prices.size()) {
-
-        if (prices[l] < prices[r]) {
-            maxProf = max(maxProf, prices[r] - prices[l]);
-        } else {
-            l = r;
+        int l = 0;
+        int r = 1;
+        int maxP = 0;
+        while (r < prices.size()) {
+            if (prices[l] > prices[r]) {
+                l = r;
+            } else {
+                maxP = max(maxP, prices[r] - prices[l]);
+            }
+            r++;
         }
-        r++;
-     }
-     return maxProf;   
+        return maxP;
     }
-
-
-    //maximium difference between prices[i] - prices[j] such that j > i => naive solution is o(n^2)
-    //two pointer method which we can use
-    //start off adjacent, tabulate the difference, store it in max, keep l fixed and move r 
-    // if l > r => l = r, r = r + 1
 };
