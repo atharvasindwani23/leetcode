67669b8da2c307ad3b87353f3b628aa3d1ea22a3
Atharva Sindwani <arvani@amazon.com>
2025-12-15 22:15:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 25.9 MB (76.48%)

--- solutions/0042-trapping-rain-water/solution.cpp
+++ solutions/0042-trapping-rain-water/solution.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int waterArea = 0;
         int l = 0;
         int r = height.size() - 1;
-        if (height.size() == 0) {
-            return 0;
-        }
-        int maxLeft = height[l];
-        int maxRight = height[r];
-
+        int maxL = height[l];
+        int maxR = height[r];
+        int sum = 0;
         while (l < r) {
             if (height[l] < height[r]) {
                 l++;
-                maxLeft = max(maxLeft, height[l]);
-                waterArea += maxLeft - height[l];
+                maxL = max(maxL, height[l]);
+                sum += (maxL - height[l]);
             } else {
                 r--;
-                maxRight = max(maxRight, height[r]);
-                waterArea += maxRight - height[r];
+                maxR = max(maxR, height[r]);
+                sum += (maxR - height[r]);
             }
         }
-        return waterArea;
+        return sum;
     }
 };
