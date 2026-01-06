11b7283dd45d07413cd505c3679e14806df6989a
Atharva Sindwani <arvani@amazon.com>
2026-01-06 07:33:39 +0000

    Sync LeetCode submission Runtime - 63 ms (80.68%), Memory - 173 MB (80.99%)

--- solutions/0146-lru-cache/solution.cpp
+++ solutions/0146-lru-cache/solution.cpp
@@ -1,79 +1,82 @@
 class LRUCache {
 
 private:
-
 struct ListNode {
-int key = 0;
-int value = 0;
-ListNode* next;
-ListNode* prev;
 
-ListNode(int k, int v) {
-    key = k;
-    value = v;   
-}
+    ListNode* next;
+    ListNode* prev;
 
+    int key;
+    int value;
+
+    ListNode(int k, int v) {
+        key = k;
+        value = v;
+    }
 };
+
+ListNode* tail_;
+ListNode* head_;
+int capacity_;
+unordered_map<int, ListNode*> keyToNode;
 
 void deleteNode(ListNode* curr) {
     ListNode* before = curr->prev;
     ListNode* after = curr->next;
+
     before->next = after;
     after->prev = before;
 }
 
-void addToTail(ListNode* curr) {
-    ListNode* before = tail->prev;
+void addNodeToTail(ListNode* curr) {
+    ListNode* before = tail_->prev;
     before->next = curr;
-    curr->next = tail;
+    curr->next = tail_;
+    tail_->prev = curr;
     curr->prev = before;
-    tail->prev = curr;
 }
-void moveToTail(ListNode* curr) {
-    deleteNode(curr);
-    addToTail(curr);
-}
-ListNode* head;
-ListNode* tail;
-int capacity_ = 0;
-unordered_map<int, ListNode*> lru;
 
+void moveToEnd(ListNode* curr) {
+    deleteNode(curr);
+    addNodeToTail(curr);
+}
 
 public:
     LRUCache(int capacity) {
         capacity_ = capacity;
-        head = new ListNode(-1, -1);
-        tail = new ListNode(-1, -1);
-        head->next = tail;
-        tail->prev = head;
+        head_ = new ListNode(-1, -1);
+        tail_ = new ListNode(-1, -1);
+
+        head_->next = tail_;
+        tail_->prev = head_;
     }
     
     int get(int key) {
-        if (lru.contains(key)) {
-            moveToTail(lru[key]);
-            return lru[key]->value;
+        if (keyToNode.count(key)) {
+            ListNode* curr = keyToNode[key];
+            moveToEnd(curr);
+            return curr->value;
         }
         return -1;
     }
     
     void put(int key, int value) {
-        if (lru.contains(key)) {
-            ListNode* currNode = lru[key];
-            currNode->value = value;
-            moveToTail(currNode);
+        if (keyToNode.count(key)) {
+            ListNode* node = keyToNode[key];
+            node->value = value;
+            moveToEnd(node);
             return;
         }
-        if (capacity_ == lru.size()) {
-            ListNode* evict = head->next;
-            int k  = evict->key;
-            deleteNode(evict);
-            lru.erase(k);
-            delete evict;
+        if (capacity_ == keyToNode.size()) {
+            ListNode* deleted = head_->next;
+            keyToNode.erase(deleted->key);
+            deleteNode(deleted);
+            delete deleted;
         }
         ListNode* newNode = new ListNode(key, value);
-        addToTail(newNode);
-        lru[key] = newNode;
-    } 
+        addNodeToTail(newNode);
+        keyToNode[key] = newNode;
+    }
 };
 
 /**
