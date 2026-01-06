9cfc2d1212b0dcc7c83261d535ecd2869a936485
Atharva Sindwani <arvani@amazon.com>
2026-01-06 07:10:45 +0000

    Sync LeetCode submission Runtime - 4 ms (45.46%), Memory - 18.2 MB (33.20%)

--- solutions/0539-minimum-time-difference/solution.cpp
+++ solutions/0539-minimum-time-difference/solution.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        vector<int> minutes(timePoints.size());
-        
-        // Convert times to minutes
-        for (int i = 0; i < timePoints.size(); ++i) {
-            int h = stoi(timePoints[i].substr(0, 2));
-            int m = stoi(timePoints[i].substr(3));
-            minutes[i] = h * 60 + m;
+        vector<int> total_minutes;
+
+        for (int i = 0; i < timePoints.size(); i++) {
+            string time = timePoints[i];
+
+            int hours = stoi(time.substr(0, 2));
+            int minutes = stoi(time.substr(3));
+
+            total_minutes.push_back(60 * hours + minutes);
+        }
+        sort(total_minutes.begin(), total_minutes.end());
+
+        int firstMin = INT_MAX;
+
+        for (int i = 1; i < total_minutes.size(); i++) {
+            firstMin = min(firstMin, total_minutes[i] - total_minutes[i - 1]);
         }
 
-        // Sort times in ascending order
-        sort(minutes.begin(), minutes.end());
-
-        // Find minimum difference across adjacent elements
-        int minDiff = INT_MAX;
-        for (int i = 0; i < minutes.size() - 1; ++i) {
-            minDiff = min(minDiff, minutes[i + 1] - minutes[i]);
-        }
-
-        // Consider the circular difference between last and first element
-        minDiff = min(minDiff, 24 * 60 - minutes.back() + minutes.front());
-
-        return minDiff;
+        return min(firstMin, 24 * 60 - total_minutes.back() + total_minutes.front());
     }
+
+    //logic is to convert everything in minutes sort it and then look at the difference between everything. since time is circular the only difference you will really care about is the one between the last and the first element.
 };
