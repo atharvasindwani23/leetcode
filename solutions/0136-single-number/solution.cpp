bdeaabe2247473ed3e4615fd8de25f3e75be5a82
Atharva Sindwani <arvani@amazon.com>
2026-01-06 08:57:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.7 MB (57.66%)

--- solutions/0136-single-number/solution.cpp
+++ solutions/0136-single-number/solution.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        std:: map<int, int> num_count;
-        for (unsigned int i = 0; i < nums.size(); i++) {
-             if (num_count.contains(nums[i])) {
-                num_count[nums[i]] = 2;
-             } else {
-                num_count[nums[i]] = 1;
-             }
-        }
+        int result = 0;
+
         for (int num : nums) {
-            if (num_count[num] == 1) {
-                return num;
-            }
+            result ^= num;
         }
-        return -1;
+        return result;
     }
 
 };
