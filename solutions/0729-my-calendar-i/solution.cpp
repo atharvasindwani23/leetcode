62d0402c03dd7ef570a37880b90baf4b79d0b433
Atharva Sindwani <arvani@amazon.com>
2026-02-18 22:21:32 +0000

    Sync LeetCode submission Runtime - 8 ms (91.19%), Memory - 44.2 MB (29.71%)

--- solutions/0729-my-calendar-i/solution.cpp
+++ solutions/0729-my-calendar-i/solution.cpp
@@ -1,34 +1,21 @@
 class MyCalendar {
 public:
-    set<pair<int,int>> bookings;
-    MyCalendar() {
-        
-    }
-    
-    bool book(int startTime, int endTime) {
-        if (bookings.size() == 0) {
-            bookings.insert({startTime, endTime});
-            return true;
+    set<pair<int,int>> calendar;
+
+    bool book(int start, int end) {
+        auto it = calendar.lower_bound({start, end}); // first interval with start' >= start
+
+        // check overlap with next interval
+        if (it != calendar.end() && it->first < end) return false;
+
+        // check overlap with previous interval
+        if (it != calendar.begin()) {
+            auto prevIt = prev(it);
+            if (prevIt->second > start) return false;
         }
-        pair<int,int> currBooking = {startTime, endTime};
-        auto it = bookings.lower_bound(currBooking);
-        auto temp = it;
-        // if (it == bookings.end()) {
-        //     bookings.insert(currBooking);
-        //     return true;
-        // }
-        //(a,b), (c,d) => max(a,c) < min(b,d)
-        --it;
-        if (min(endTime,it->second) > max(startTime, it->first) || min(endTime,temp->second) > max(startTime, temp->first)) {
-            return false;
-        }
-        bookings.insert(currBooking);
+
+        calendar.insert({start, end});
         return true;
     }
 };
 
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(startTime,endTime);
- */
