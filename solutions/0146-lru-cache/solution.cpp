262f46972bbb593495658f00c3d2038a7ab67b74
Atharva Sindwani <arvani@amazon.com>
2026-03-26 23:57:44 +0000

    Sync LeetCode submission Runtime - 57 ms (88.70%), Memory - 173.1 MB (68.38%)

--- solutions/0146-lru-cache/solution.cpp
+++ solutions/0146-lru-cache/solution.cpp
@@ -1,24 +1,25 @@
 class LRUCache {
 
-private:
 struct ListNode {
 
     ListNode* next;
     ListNode* prev;
-
-    int key;
-    int value;
-
+    int key = 0;
+    int value = 0;
     ListNode(int k, int v) {
         key = k;
         value = v;
     }
 };
 
-ListNode* tail_;
-ListNode* head_;
-int capacity_;
+ListNode* head;
+ListNode* tail;
+int capacity_ = 0;
+
 unordered_map<int, ListNode*> keyToNode;
+
+//is to delete curr posn
+//add to end
 
 void deleteNode(ListNode* curr) {
     ListNode* before = curr->prev;
@@ -28,54 +29,54 @@ void deleteNode(ListNode* curr) {
     after->prev = before;
 }
 
-void addNodeToTail(ListNode* curr) {
-    ListNode* before = tail_->prev;
+void addToEnd(ListNode* curr) {
+    ListNode* after = tail;
+    ListNode* before = tail->prev;
+    curr->next = after;
+    after->prev = curr;
     before->next = curr;
-    curr->next = tail_;
-    tail_->prev = curr;
     curr->prev = before;
 }
 
-void moveToEnd(ListNode* curr) {
+void move(ListNode* curr) {
     deleteNode(curr);
-    addNodeToTail(curr);
+    addToEnd(curr);
 }
 
 public:
     LRUCache(int capacity) {
-        capacity_ = capacity;
-        head_ = new ListNode(-1, -1);
-        tail_ = new ListNode(-1, -1);
-
-        head_->next = tail_;
-        tail_->prev = head_;
+       capacity_ = capacity;
+       head = new ListNode(-1, -1);
+       tail = new ListNode(-1, -1);
+       head->next = tail;
+       tail->prev = head; 
     }
     
     int get(int key) {
-        if (keyToNode.count(key)) {
-            ListNode* curr = keyToNode[key];
-            moveToEnd(curr);
-            return curr->value;
+        if (!keyToNode.count(key)) {
+            return -1;
         }
-        return -1;
+        ListNode* curr = keyToNode[key];
+        move(curr);
+        return curr->value;
     }
     
     void put(int key, int value) {
         if (keyToNode.count(key)) {
-            ListNode* node = keyToNode[key];
-            node->value = value;
-            moveToEnd(node);
-            return;
+            ListNode* curr = keyToNode[key];
+            curr->value = value;
+            move(curr);
+        } else {
+            ListNode* newKey = new ListNode(key, value);
+            addToEnd(newKey);
+            keyToNode[key] = newKey;
         }
-        if (capacity_ == keyToNode.size()) {
-            ListNode* deleted = head_->next;
-            keyToNode.erase(deleted->key);
-            deleteNode(deleted);
-            delete deleted;
-        }
-        ListNode* newNode = new ListNode(key, value);
-        addNodeToTail(newNode);
-        keyToNode[key] = newNode;
+        if (keyToNode.size() > capacity_) {
+                ListNode* lru = head->next;   // least recently used
+                deleteNode(lru);
+                keyToNode.erase(lru->key);
+                delete lru;
+            }
     }
 };
 
