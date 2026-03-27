6f051316265eb9a976911599b33b72e2ea64c9ec
Atharva Sindwani <arvani@amazon.com>
2026-03-27 01:33:41 +0000

    Sync LeetCode submission Runtime - 41 ms (58.85%), Memory - 110.5 MB (61.72%)

--- solutions/0947-online-election/solution.cpp
+++ solutions/0947-online-election/solution.cpp
@@ -1,25 +1,42 @@
 class TopVotedCandidate {
 public:
-    vector<int> times;
-    vector<int> leaders;
-    unordered_map<int, int> count;
+    vector<int> voteTimes;
+    vector<int> leaderAtIndex;
 
     TopVotedCandidate(vector<int>& persons, vector<int>& times) {
-        this->times = times;
+        voteTimes = times;
+
+        unordered_map<int, int> freq;
         int leader = -1;
-        for (int i = 0; i < persons.size(); ++i) {
-            count[persons[i]]++;
-            if (leader == -1 || count[persons[i]] >= count[leader]) {
+        int maxVotes = 0;
+
+        for (int i = 0; i < persons.size(); i++) {
+            freq[persons[i]]++;
+
+            // tie goes to the most recent vote
+            if (freq[persons[i]] >= maxVotes) {
+                maxVotes = freq[persons[i]];
                 leader = persons[i];
             }
-            leaders.push_back(leader);
+
+            leaderAtIndex.push_back(leader);
         }
     }
-
+    
     int q(int t) {
-        // binary search: find last index where times[i] <= t
-        int idx = upper_bound(times.begin(), times.end(), t) - times.begin() - 1;
-        return leaders[idx];
+        int left = 0;
+        int right = voteTimes.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (voteTimes[mid] <= t) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return leaderAtIndex[right];
     }
 };
-
