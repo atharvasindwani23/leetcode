935e480fabfbf53722b94ff1e86a1a557e88e3bd
Atharva Sindwani <arvani@amazon.com>
2026-04-12 03:58:06 +0000

    Sync LeetCode submission Runtime - 21 ms (69.29%), Memory - 66 MB (39.31%)

--- solutions/1582-design-browser-history/solution.cpp
+++ solutions/1582-design-browser-history/solution.cpp
@@ -3,59 +3,73 @@ class BrowserHistory {
 private:
 
 struct ListNode {
-    ListNode* next;
-    ListNode* prev;
-    string url;
-    ListNode(string str) {
-        url = str;
-    }
+
+ListNode* next;
+ListNode* prev;
+string url_;
+ListNode(string url) {
+    url_ = url;
+}
+
 };
 
-ListNode* head;
-ListNode* tail;
-ListNode* curr;
+ListNode* head_;
+ListNode* tail_;
+ListNode* curr_;
 
 public:
     BrowserHistory(string homepage) {
-        head = new ListNode("");
-        tail = new ListNode("");
-        head->next = tail;
-        tail->prev = head;
-
+        head_ = new ListNode("");
+        tail_ = new ListNode("");
         ListNode* first = new ListNode(homepage);
-        head->next = first;
-        first->prev = head;
-        first->next = tail;
-        tail->prev = first;
-        curr = first;
-    }
+
+        head_->next = first;
+        first->prev = head_;
+        first->next = tail_;
+        tail_->prev = first;
+
+        curr_ = first;
+
+    }   
     
     void visit(string url) {
-        ListNode* new_url = new ListNode(url);
-        new_url->next = tail;
-        tail->prev = new_url;
-        curr->next = new_url;
-        new_url->prev = curr;
-
-        curr = new_url;
+        ListNode* before = tail_->prev;
+        ListNode* newNode = new ListNode(url);
+        newNode->next = tail_;
+        tail_->prev = newNode;
+        before->next = newNode;
+        newNode->prev = before;
+        curr_->next = newNode;
+        newNode->prev = curr_;
+        curr_ = newNode;
     }
     
     string back(int steps) {
         int count = 0;
-        while (curr->prev != head && count != steps) {
-            curr = curr->prev;
+
+        ListNode* flag = curr_;
+
+        while (flag != head_->next && count != steps) {
+            flag = flag->prev;
             count++;
         }
-            return curr->url;
-        }
+            curr_ = flag;
+            return flag->url_;
+        return "";
+    }
     
     string forward(int steps) {
-         int count = 0;
-        while (curr->next != tail && count != steps) {
-            curr = curr->next;
+        int count = 0;
+
+        ListNode* flag = curr_;
+
+        while (flag != tail_->prev && count != steps) {
+            flag = flag->next;
             count++;
         }
-            return curr->url;
+
+            curr_ = flag;
+            return flag->url_;
     }
 };
 
